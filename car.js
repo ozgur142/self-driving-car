c0e25ac8259dadbdba1225a746fb4c2a0500119f
Ozgur <ozgurcandogan@gmail.com>
2023-07-04 23:21:20 +0300

    car

--- car.js
+++ car.js
@@ -4,16 +4,6 @@ class Car{
         this.y = y;
         this.width = width;
         this.height = height;
-
-        this.speed = 0;
-        this.acceleration = 0.2;
-        if (controlType == "KEYS"){   
-            this.maxSpeed = 4;
-            this.sensor = new Sensor(this);
-        }else if(controlType == "DUMMY"){
-            this.maxSpeed = 3;
-        }
-
         this.reverseMaxSpeed = -1.8;
         this.friction = 0.04;
 
@@ -22,7 +12,29 @@ class Car{
 
         this.damaged = false;
 
+        this.useBrain=controlType=="AI";
+
+        
+
+        this.speed = 0;
+        this.acceleration = 0.2;
+        if (controlType == "KEYS"){   
+            this.maxSpeed = 4;
+            this.sensor = new Sensor(this);
+        }else if(controlType == "AI"){
+            this.maxSpeed = 4;
+            this.sensor = new Sensor(this);
+            this.brain=new NeuralNetwork(
+                [this.sensor.rayCount,6,4]
+            );
+        }else if(controlType == "DUMMY"){
+            this.maxSpeed = 3;
+        }
+
+
+
         this.controls = new Controls(controlType);
+        
     }
 
     update(roadBorders, traffic) {
@@ -33,6 +45,15 @@ class Car{
         }
         if (this.sensor){
             this.sensor.update(roadBorders, traffic);
+            const offsets = this.sensor.readings.map(s=>s==null?0:1-s.offset);
+            const outputs=NeuralNetwork.feedForward(offsets,this.brain);
+
+            if(this.useBrain){
+                this.controls.forward = outputs[0];
+                this.controls.left = outputs[1];
+                this.controls.right = outputs[2];
+                this.controls.reverse = outputs[3];
+            }
         }
     }
 
@@ -118,7 +139,7 @@ class Car{
         this.y -= Math.cos(this.angle)*this.speed;
     }
 
-    draw(ctx, color){
+    draw(ctx, color, drawSensor=false){
         if (this.damaged){
             ctx.fillStyle = "gray";
         }else{
@@ -132,7 +153,7 @@ class Car{
         }
         ctx.fill();
 
-        if(this.sensor){
+        if(this.sensor && drawSensor){
             this.sensor.draw(ctx);
         }
     }
